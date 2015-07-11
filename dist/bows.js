f7066a97dfa085d40b6de175b2622399d103b364
Philip Roberts <phil@latentflip.com>
2015-07-11 17:59:26 +0100

    Build dist.

--- dist/bows.js
+++ dist/bows.js
@@ -1,6 +1,9 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.bows=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function() {
   function checkColorSupport() {
+    if (typeof window === 'undefined' || typeof navigator === 'undefined') {
+      return false;
+    }
     var chrome = !!window.chrome,
         firefox = /firefox/i.test(navigator.userAgent),
         firefoxVersion;
@@ -33,7 +36,7 @@
       colorsSupported = ls.debugColors || checkColorSupport(),
       bows = null,
       debugRegex = null,
-      invertRegex = false
+      invertRegex = false,
       moduleColorsMap = {};
 
   if (debug && debug[0] === '!' && debug[1] === '/') {
