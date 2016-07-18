175bcba8e6b356d5af9e2cc36b046107816568a1
Philip Roberts <phil@latentflip.com>
2016-07-18 11:33:42 +0100

    Build

--- dist/bows.js
+++ dist/bows.js
@@ -1,4 +1,5 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.bows=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (process){
 (function() {
   function checkColorSupport() {
     if (typeof window === 'undefined' || typeof navigator === 'undefined') {
@@ -7,7 +8,8 @@
 
     var chrome = !!window.chrome,
         firefox = /firefox/i.test(navigator.userAgent),
-        firefoxVersion;
+        firefoxVersion,
+        electron = process && process.versions && process.versions.electron;
 
     if (firefox) {
         var match = navigator.userAgent.match(/Firefox\/(\d+\.\d+)/);
@@ -15,7 +17,7 @@
             firefoxVersion = Number(match[1]);
         }
     }
-    return chrome || firefoxVersion >= 31.0;
+    return chrome || firefoxVersion >= 31.0 || electron;
   }
 
   var yieldColor = function() {
@@ -109,7 +111,8 @@
   }
 }).call();
 
-},{"andlog":2}],2:[function(require,module,exports){
+}).call(this,require('_process'))
+},{"_process":3,"andlog":2}],2:[function(require,module,exports){
 // follow @HenrikJoreteg and @andyet if you like this ;)
 (function () {
     var inNode = typeof window === 'undefined',
@@ -139,6 +142,71 @@
         window.console = out;
     }
 })();
+
+},{}],3:[function(require,module,exports){
+// shim for using process in browser
+
+var process = module.exports = {};
+
+process.nextTick = (function () {
+    var canSetImmediate = typeof window !== 'undefined'
+    && window.setImmediate;
+    var canPost = typeof window !== 'undefined'
+    && window.postMessage && window.addEventListener
+    ;
+
+    if (canSetImmediate) {
+        return function (f) { return window.setImmediate(f) };
+    }
+
+    if (canPost) {
+        var queue = [];
+        window.addEventListener('message', function (ev) {
+            var source = ev.source;
+            if ((source === window || source === null) && ev.data === 'process-tick') {
+                ev.stopPropagation();
+                if (queue.length > 0) {
+                    var fn = queue.shift();
+                    fn();
+                }
+            }
+        }, true);
+
+        return function nextTick(fn) {
+            queue.push(fn);
+            window.postMessage('process-tick', '*');
+        };
+    }
+
+    return function nextTick(fn) {
+        setTimeout(fn, 0);
+    };
+})();
+
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+}
+
+// TODO(shtylman)
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
 
 },{}]},{},[1])(1)
 });
