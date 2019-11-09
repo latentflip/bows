9c91a705f2dbf3733825d7a3e3dbdd8210d05e13
Philip Roberts <latentflip@github.com>
2019-11-09 10:13:09 +0100

    Add test for localStorage inaccessible

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ var runTestPage = require('./helpers/runTestPage');
 var scripts = [
     'test/enabled.html',
     'test/disabled.html',
+    'test/no-local-storage.html',
     'test/alternative-key.html',
     'test/regex.html',
     'test/not-regex.html',
