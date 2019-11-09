6b0d6b4117e7559a2c1f44c8e3bcc99694015f79
Philip Roberts <latentflip@github.com>
2019-11-09 10:50:29 +0100

    Run prettier

--- test/index.js
+++ test/index.js
@@ -1,41 +1,44 @@
 var runTestPage = require('./helpers/runTestPage');
 
 var scripts = [
-    'test/enabled.html',
-    'test/disabled.html',
-    'test/no-local-storage.html',
-    'test/alternative-key.html',
-    'test/regex.html',
-    'test/not-regex.html',
-    'test/color-map.html',
-    'test/separator.html',
-    'test/no-separator.html',
-    'test/padLength.html',
-    'test/no-padding.html',
-    'test/args.html'
+  'test/enabled.html',
+  'test/disabled.html',
+  'test/no-local-storage.html',
+  'test/alternative-key.html',
+  'test/regex.html',
+  'test/not-regex.html',
+  'test/color-map.html',
+  'test/separator.html',
+  'test/no-separator.html',
+  'test/padLength.html',
+  'test/no-padding.html',
+  'test/args.html'
 ];
 
-async function runAllScripts () {
-    const results = {passed: [], failed: []}
+async function runAllScripts() {
+  const results = { passed: [], failed: [] };
 
-    for (const script of scripts) {
-        console.log(`\n--- Running ${script} ---`)
-        const passed = await runTestPage(script)
-        if (passed) {
-            results.passed.push(script)
-        } else {
-            results.failed.push(script)
-        }
-    }
-
-    if (results.failed.length === 0) {
-        console.log('All test suites passed');
+  for (const script of scripts) {
+    console.log(`\n--- Running ${script} ---`);
+    const passed = await runTestPage(script);
+    if (passed) {
+      results.passed.push(script);
     } else {
-        console.log(`${results.failed.length} of ${results.failed.length + results.passed.length} suites failed.`)
+      results.failed.push(script);
     }
+  }
 
-    const exitCode = results.failed.length === 0 ? 0 : 1
-    process.exit(exitCode)
+  if (results.failed.length === 0) {
+    console.log('All test suites passed');
+  } else {
+    console.log(
+      `${results.failed.length} of ${results.failed.length +
+        results.passed.length} suites failed.`
+    );
+  }
+
+  const exitCode = results.failed.length === 0 ? 0 : 1;
+  process.exit(exitCode);
 }
 
-runAllScripts()
+runAllScripts();
